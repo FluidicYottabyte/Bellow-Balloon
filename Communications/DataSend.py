4f6006554acddc558668aa083ca33744ef6af11a
FluidicYottabyte <paxton@paulos.net>
2024-05-10 10:37:06 -0700

    Created the encoding method

--- Communications/DataSend.py
+++ Communications/DataSend.py
@@ -9,43 +9,81 @@ Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
 Author: Brent Rubell for Adafruit Industries
 """
 import time
-import busio
-from digitalio import DigitalInOut, Direction, Pull
-import board
+import os
+from queue import PriorityQueue
+
+path = os.getcwd()
+
+print(os.path.dirname(os.path.dirname(os.path.dirname(path))))
+
+#checks if it is on pi or not. Remove completely on launch
+
+onPi = False
+
+if os.path.dirname(os.path.dirname(os.path.dirname(path))) == "home/":
+    print("on Pi")
+    import busio
+    from digitalio import DigitalInOut, Direction, Pull
+    import board
+
+    import adafruit_rfm9x
+
+    # Configure RFM9x LoRa Radio
+    CS = DigitalInOut(board.CE1)
+    RESET = DigitalInOut(board.D25)
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+    Rad = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+
+    Rad.tx_power = 23
+    Rad.ack_wait = 1
+    Rad.ack_retries = 2
+    Rad.coding_rate = 5
+    
+    Rad.auto_agc = True
+    onPi = True
+
+outgoing = PriorityQueue()
+
 from queue import PriorityQueue
 # Import the SSD1306 module.
 # Import the RFM9x radio module.
-import adafruit_rfm9x
 
-# Configure RFM9x LoRa Radio
-CS = DigitalInOut(board.CE1)
-RESET = DigitalInOut(board.D25)
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-Rad = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
 
-Rad.tx_power = 23
-Rad.ack_wait = 1
-Rad.ack_retries = 2
-Rad.coding_rate = 5
-outgoing = PriorityQueue()
-Rad.auto_agc = True
+
 
 class Radio:
     
     def __init__(self):
-        print("Begin radio object")
-        Rad.reset()
-        Rad.spreading_factor = 6
-        print("Valid bandwidth:"+str(Rad.bw_bins))
+        if onPi:
+            print("Begin radio object")
+            Rad.reset()
+            Rad.spreading_factor = 6
+            print("Valid bandwidth:"+str(Rad.bw_bins))
         
     def test(self):
         return(Rad.send_with_ack(b"TEST"))
         
     def getQueue(self):
         return(outgoing)
+    
+    def addQueue(self,priority,object):
+        outgoing.put(object,priority)
         
+    def send(self):
+        SizeStat = not (outgoing.empty())
+        print(SizeStat)
+        if SizeStat:
+            CurrTask = outgoing.get()
+            print(CurrTask)
         
-        
+            with open(os.path.join(os.path.join(os.getcwd(), "Communications"),CurrTask), "rb") as image:
+                f = image.read()
+                b = bytearray(f)
+                print(b)
+            return True
+        else:
+            return False
+
     #In the event of loss of contact, this function will run through all possible fixes
     
     def attemptFix(self):
--- OS/main.py
+++ OS/main.py
@@ -1,15 +1,35 @@
 
 import threading
 import sys
-sys.path.insert(0, '/home/Quincy/Bellow-Balloon')
+import time
+import os
+
+path = os.getcwd()
+sys.path.insert(0, path)
+
+print(path)
+
 from Communications import DataSend
 
 Radio = DataSend.Radio()
 
 Radio.getQueue()
+Radio.addQueue(1,"TestIMG1.jpg")
+
+def RadioControlSend():
+    print("Radio control thread initialized")
+    move = True
+    while move:
+        print("Acting radio...")
+        success = Radio.send()
+        if not success:
+            move = False
+
+
+thread1 = threading.Thread(target=RadioControlSend)
+thread1.start()
 
 """
-thread1 = threading.Thread(target=)
 thread2 = threading.Thread(target=)
 thread3 = threading.Thread(target=)
 thread4 = threading.Thread(target=)"""
